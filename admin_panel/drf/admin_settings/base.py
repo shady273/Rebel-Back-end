422f16c45f8a4507e1d0fcdcf3f646678ac65623
taras <taras2730@gmail.com>
2024-01-02 22:45:33 +0200

    fixing admin panel

--- admin_panel/drf/admin_settings/base.py
+++ admin_panel/drf/admin_settings/base.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 
 
 class BaseAdmin(admin.ModelAdmin):
+    def changelist_view(self, request, extra_context=None):
+        obj = self.model.objects.first()
+        if obj:
+            return redirect('admin:%s_%s_change' % (self.model._meta.app_label, self.model._meta.model_name), obj.id)
+        else:
+            return super().add_view(request, extra_context)
+
     def has_add_permission(self, request):
         if self.model.objects.exists():
             return False
         return super().has_add_permission(request)
+
+
+class BasePhotoInline(admin.StackedInline):
+    def get_extra(self, request, obj=None, **kwargs):
+        if obj is not None:
+            return 0
+        return 1
